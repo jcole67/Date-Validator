7d565201ae64932339bfe9a30f10cfb1aaedf06b
Jordan Cole <55559719+jcole67@users.noreply.github.com>
2021-02-23 16:19:36 -0800

    Update Date-Validator.cpp

--- Date-Validator.cpp
+++ Date-Validator.cpp
@@ -1,36 +1,40 @@
+//CS_117_20895SP21V
+//Assignment 1
+//Jordan Cole
+
 #include <iostream>
 
 using namespace std;
 
-bool isValid(int& month, int& day, int& year);
+bool isValid(int& month, int& day, int& year);                                  //function to validate or correct user-input date
 
 int main()
 {
-    int month, day, year;
-    char slash;
+    int month, day, year;                                                       //declare vars for user-input data
+    char slash;                                                                 //declare dummy character to accept '/'
     
     cout << "Enter a date: ";
-    cin >> month >> slash >> day >> slash >> year;
+    cin >> month >> slash >> day >> slash >> year;                              //receive user-input data
     
-    if ( (month >= 1) && (month <= 12) )
+    if ( (month >= 1) && (month <= 12) )                                        //validate that valid month was entered
     {
-        if ( (day >= 1) && (day <= 59) )
+        if ( (day >= 1) && (day <= 59) )                                        //validate that valid day was entered
         {
-            if (isValid(month, day, year) == true)
-                cout << "Date validated.";
+            if (isValid(month, day, year) == true)                              //call function with user-input data
+                cout << "Date validated.";                                      //print correct date confirmation
             else
-                cout << "Invalid date entered. Changed to " << month << "/" << day << "/" << year;
+                cout << "Invalid date entered. Changed to " << month << "/" << day << "/" << year;  //print correction of an erroneous date
         }
         else
-            cout << "Please enter a vaild day (1 - 59). Try again.";
+            cout << "Please enter a vaild day. Try again.";            //error message for invalid day
     }
     else
-        cout << "Please enter a vaild month (1 - 12). Try again.";
+        cout << "Please enter a vaild month. Try again.";              //error message for invalid month
     
     return 0;
 }
 
-bool isValid (int& month, int& day, int& year)
+bool isValid (int& month, int& day, int& year)                                  //function verifies user-input date using known month lengths
 {
     switch (month)
     {
@@ -42,7 +46,7 @@ bool isValid (int& month, int& day, int& year)
                 month = (month + 1);
                 return false;
             break;
-        case 2:
+        case 2:                                                                 //February accounts for leap years
             if ( (year % 400 == 0) || ( (year % 4 == 0) && (year % 100 != 0) ) )
             {
                 if (day <= 29)
@@ -134,7 +138,7 @@ bool isValid (int& month, int& day, int& year)
                 month = (month + 1);
                 return false;
             break;
-        case 12:
+        case 12:                                                                //December loops month back around to January when over 31 days
             if (day <= 31)
                 return true;
             else
